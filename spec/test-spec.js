bc79c36025d925a8458e5d09f59c05f27575f232
Alex Scown <alex.scown@hpe.com>
2016-04-07 12:00:01 +0100

    Add spec which will never run

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -2,6 +2,10 @@ describe('A spec with no expectations', function() {
 
     it('should return 1', function() {
         var a = code();
-    })
+    });
+
+    it('should never reach this spec due to the error', function() {
+        expect(code()).toBe(1);
+    });
 
 });
